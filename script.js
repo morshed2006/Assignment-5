521998cdd44278885ba67211b6ce341f56bfd0bf
morshed2006 <hussainmorshed141@gmail.com>
2025-08-28 09:45:08 -0400

    button update

--- script.js
+++ script.js
@@ -15,3 +15,58 @@ for (const heart of hearts) {
 		setHeartNumber.innerText = counter;
 	});
 }
+//Call button funtions
+
+const callButton = document.querySelectorAll(".call-btn");
+const coins = document.getElementById("coins").innerText;
+const coinsId = document.getElementById("coins");
+const reduceCoins = 20;
+
+for (const button of callButton) {
+	button.addEventListener("click", function () {
+		const card = button.closest(".parent-card");
+
+		if (card) {
+			serviceName = card.querySelector("h1").innerText || "service";
+			serviceNumber = card.querySelector("h1.text-3xl").innerText || "Number";
+		}
+
+		alert(Service Name: $ { serviceName } \ nCalling... $ { serviceNumber });
+
+		let currentCoins = parseInt(coinsId.innerText, 10);
+
+		if (currentCoins >= reduceCoins) {
+			currentCoins -= reduceCoins;
+			coinsId.innerText = currentCoins;
+		} else {
+			alert("Not enough coins");
+		}
+
+		const data = {
+			name: serviceName,
+			number: serviceNumber,
+			date: new Date().toLocaleTimeString(),
+		};
+
+		historyData.push(data);
+
+		const historyContainer = document.getElementById("history");
+        historyContainer.innerText= ''
+
+		for (const data of historyData) {
+			const div = document.createElement("div");
+			div.innerHTML = `
+        <div class="flex justify-between items-center p-3 rounded-xl bg-[#fafafa] mt-3">
+            <div >
+                <h2 class="text-lg mb-3 font-bold">${data.name}</h2>
+                <p class="text-gray-500 font-semibold">${data.number}</p>
+            </div>
+            <div>
+                <p class="font-semibold" >${data.date}</p>
+            </div>
+        </div>
+    `;
+			historyContainer.appendChild(div);
+		}
+	});
+}
